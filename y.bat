c3e089cc03648fdbfd8d1fa69982ee077510e261
Vi925 <118710359+Vi925@users.noreply.github.com>
2025-04-05 05:22:37 +0530

    Create y.bat

---
+++ y.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+
+echo Copying text to clipboard...
+echo lhflfhaldfhkdjafhkajhfdkd | clip
+
+echo Waiting an additional 2 seconds...
+ping -n 3 127.0.0.1 > nul
+
+echo Debug: Running PowerShell automation...
+powershell -ExecutionPolicy Bypass -File "%~dp0automation2.ps1"
+
+echo Debug: Script execution completed.
+pause
