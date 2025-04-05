56258e2d80a61276da38bb96c08d900295cea7af
Vi925 <118710359+Vi925@users.noreply.github.com>
2025-04-05 05:49:58 +0530

    Update l.bat

--- l.bat
+++ l.bat
@@ -3,12 +3,6 @@ setlocal ENABLEDELAYEDEXPANSION
 
 
 
-echo Copying text to clipboard...
-echo lhflfhaldfhkdjafhkajhfdkd | clip
-
-echo Waiting an additional 2 seconds...
-ping -n 3 127.0.0.1 > nul
-
 echo Debug: Running PowerShell automation...
 powershell -ExecutionPolicy Bypass -File "%~dp0automation3.ps1"
 
