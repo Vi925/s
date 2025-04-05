9f98f4f3b67738f1860cc0681eb8ea6ca83ad75b
Vi925 <118710359+Vi925@users.noreply.github.com>
2025-04-05 05:35:52 +0530

    Update r.bat

--- r.bat
+++ r.bat
@@ -8,7 +8,7 @@ echo Waiting an additional 2 seconds...
 ping -n 3 127.0.0.1 > nul
 
 echo Debug: Running PowerShell automation...
-powershell -ExecutionPolicy Bypass -File "%~dp0automation.ps1"
+powershell -ExecutionPolicy Bypass -File "%~dp0automation4.ps1"
 
 echo Debug: Script execution completed.
 pause
