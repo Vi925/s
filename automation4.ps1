b2fdb4a71c9f77f40ad06a7b1d1995f098689ce4
Vi925 <118710359+Vi925@users.noreply.github.com>
2025-04-05 05:40:00 +0530

    Update automation4.ps1

--- automation4.ps1
+++ automation4.ps1
@@ -1,1 +1,22 @@
 
+
+# Copy js to clipboard
+$jsContent = @"
+yacc -d yourfile.y   
+lex yourfile.l       
+gcc lex.yy.c y.tab.c -o your_program -ly
+"@
+
+Set-Clipboard -Value $jsContent
+
+
+
+
+
+
+
+
+
+
+
+Write-Output "done."
